669f1007d623fc60750e9dc1a209dd96da365ce9
Dotsworthy <dotsworthy@gmail.com>
2020-08-21 15:40:07 +0100

    added svg file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo-r.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
 
+      </header>
     </div>
   );
 }
